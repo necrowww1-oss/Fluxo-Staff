b113a164b58eab02515f4fbb374b6e75a0e3cd97
necrowww1-oss <necrowww1@gmail.com>
2026-03-21 23:00:53 -0300

    Delete src directory

--- src/main.jsx
+++
@@ -1,4 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './fluxo-staff.jsx'
-ReactDOM.createRoot(document.getElementById('root')).render(<React.StrictMode><App /></React.StrictMode>)
